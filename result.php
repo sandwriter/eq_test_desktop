1ffbbf43c1365b464bc8bea36dceaaebc504618c
Wenjie Sha <wenjie@freedailyknowledge.com>
2015-04-16 01:06:48 -0400

    Add a result.php page.

---
+++ result.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title>EQ Test Score</title>
+  </head>
+  <body>
+
+    <!--
+        Start of DoubleClick Floodlight Tag: Please do not remove
+        Activity name of this tag: Wenjie_floodlight_quiz_result
+        URL of the webpage where the tag is expected to be placed: http://quiz.freedailyknowledge.com/result.php?
+        This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+        Creation Date: 04/14/2015
+      -->
+    <script type="text/javascript">
+      var axel = Math.random() + "";
+      var a = axel * 10000000000000;
+      document.write('<iframe src="http://4817019.fls.doubleclick.net/activityi;src=4817019;type=result;cat=wenji0;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+    </script>
+    <noscript>
+      <iframe src="http://4817019.fls.doubleclick.net/activityi;src=4817019;type=result;cat=wenji0;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+    </noscript>
+    <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
+
+
+    <!--
+        Start of DoubleClick Floodlight Tag: Please do not remove
+        Activity name of this tag: quiz pixel
+        URL of the webpage where the tag is expected to be placed: http://quiz.freedailyknowledge.com
+        This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+        Creation Date: 04/13/2015
+      -->
+    <script type="text/javascript">
+      var axel = Math.random() + "";
+      var a = axel * 10000000000000;
+      document.write('<iframe src="https://4767920.fls.doubleclick.net/activityi;src=4767920;type=invmedia;cat=cbcorxem;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+    </script>
+    <noscript>
+      <iframe src="https://4767920.fls.doubleclick.net/activityi;src=4767920;type=invmedia;cat=cbcorxem;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+    </noscript>
+    <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
+    <p>Sorry, we encounter some internal error! We are working on it now!</p>  </body>
+</html>

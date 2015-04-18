849696da03a033b147c98ee78b9f54e77cda8e03
Wenjie Sha <wenjie@freedailyknowledge.com>
2015-04-17 23:35:36 -0400

    Insert ip, datetime into database.

--- result.php
+++ result.php
@@ -1,8 +1,28 @@
 <html>
-  <head>
+  <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
     <title>EQ Test Score</title>
   </head>
   <body>
+  <!-- Google Code for eq quiz result adwords Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 954109050;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "F_iiCPv0rVsQ-pj6xgM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/954109050/?label=F_iiCPv0rVsQ-pj6xgM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
 
     <!--
         Start of DoubleClick Floodlight Tag: Please do not remove
@@ -38,5 +58,34 @@
       <iframe src="https://4767920.fls.doubleclick.net/activityi;src=4767920;type=invmedia;cat=cbcorxem;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
     </noscript>
     <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
-    <p>Sorry, we encounter some internal error! We are working on it now!</p>  </body>
+
+<?php
+$servername = "localhost";
+$username = "test_user";
+$password = "asdf1234";
+$dbname = "eq_quiz";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "INSERT INTO `visitor`( `ip`, `cookie`, `datetime`) VALUES ('{$_SERVER['REMOTE_ADDR']}', 0, now())";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Sorry, we encounter some internal error! We are working on it now!";
+} else {
+    echo "SQL error.";
+}
+
+$conn->close();
+
+?>
+    </body>
+    
+    
+    
 </html>
